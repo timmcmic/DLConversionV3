26239ba4db32fb4a6bbd29158d1541ab1b7712c0
Timothy J. McMichael <timmcmic@microsoft.com>
2025-08-04 14:31:13 -0400

    Updating

--- validate-GraphContext.ps1
+++ validate-GraphContext.ps1
@@ -30,6 +30,22 @@
 
         write-objectProperties -objectToWrite $functionGraphContext
 
+        out-logfile -string "Testing graph scopes."
+
+        foreach ($scope in $msGraphScopesRequired)
+        {
+            out-logfile -string ("Testing scope: "+$scope)
+
+            if ($functionGraphContext.Scopes.contains($scope))
+            {
+                out-logfile -string "Required scope located - proceed."
+            }
+            else 
+            {
+                Out-logfile -string ("Graph Scope Required and Missing: "+$scope) -isError:$TRUE
+            }
+        }
+
         Out-LogFile -string "********************************************************************************"
         Out-LogFile -string "END validate-GraphContext"
         Out-LogFile -string "********************************************************************************"
