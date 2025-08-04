b47410c3cf154506de6b14b0b45f9c9cf58f00f7
Timothy J. McMichael <timmcmic@microsoft.com>
2025-08-04 15:44:16 -0400

    updating

--- Get-O365DLConfiguration.ps1
+++ Get-O365DLConfiguration.ps1
@@ -27,13 +27,7 @@
         Param
         (
             [Parameter(Mandatory = $true)]
-            [string]$groupSMTPAddress,
-            [Parameter(Mandatory = $false)]
-            [string]$groupTypeOverride="",
-            [Parameter(Mandatory = $false)]
-            [boolean]$isUnifiedGroup=$false,
-            [Parameter(Mandatory = $false)]
-            [boolean]$isFirstPass=$false
+            [string]$groupSMTPAddress
         )
 
         #Output all parameters bound or unbound and their associated values.
@@ -43,9 +37,6 @@
         #Declare function variables.
 
         $functionDLConfiguration=$NULL #Holds the return information for the group query.
-        $functionMailSecurity="MailUniversalSecurityGroup"
-        $functionMailDistribution="MailUniversalDistributionGroup"
-        $functionGroupType = "GroupMailbox"
 
         #Start function processing.
 
@@ -55,98 +46,12 @@
 
         #Get the recipient using the exchange online powershell session.
 
-        try
-        {
-            out-logfile -string "Obtaining Office 365 DL Configuration for evaluation."
-
-            $functionRecipient = get-o365Recipient -identity $groupSMTPAddress -errorAction STOP
-
-            out-logfile -string "Successfully obtained the Office 365 DL Configuration."
+        try {
+            $functionDLConfiguration = get-O365DistributionGroup -identity $groupSMTPAddress -errorAction STOP
         }
-        catch
-        {
-            out-logfile -string "Unable to obtain the Office 365 DL Configuration."
-            out-logfile -string $_ -isError:$TRUE
-        }
-
-        $functionRecipient = get-o365Recipient -identity $groupSMTPAddress
-
-        out-logfile -string $functionRecipient
-
-        out-logfile -string "Testing if this is the first pass for DL validation."
-
-        if ($isFirstPass -eq $TRUE)
-        {
-            out-logfile -string "This is the first pass."
-
-            if ($functionRecipient.RecipientTypeDetails -eq $functionGroupType)
-            {
-                out-logfile -string "Office 365 Recipient found is already an Office 365 Unified Group - exit." -isError:$TRUE
-            }
-            elseif (($functionRecipient.RecipientType -ne $functionMailSecurity) -and ($functionRecipient.RecipientType -ne $functionMailDistribution)) 
-            {
-                out-logfile -string "Office 365 Recipient found was not a mail universal distribution or mail universal security group - exit." -isError:$TRUE
-            }
-            else 
-            {
-                out-logfile -string "Proceed with further group evaluation - group object located in Office 365."
-            }
-    
-        }
-        else 
-        {
-            out-logfile -string "This is not the first pass."
-        }
-
-        if (($isUnifiedGroup -eq $false) -and ($functionRecipient.recipientTypeDetails -ne $functionGroupType))
-        {
-            out-logfile -string "Group is not unified use standard DL commands."
-
-            try 
-            {
-                if ($groupTypeOverride -eq "")
-                {
-                    Out-LogFile -string "Using Exchange Online to capture the distribution group."
-
-                    $functionDLConfiguration=get-O365DistributionGroup -identity $groupSMTPAddress -errorAction STOP
-                
-                    Out-LogFile -string "Original DL configuration found and recorded."
-                }
-                elseif ($groupTypeOverride -eq "Security")
-                {
-                    Out-logfile -string "Using Exchange Online to capture distribution group with filter security"
-
-                    $functionDLConfiguration=get-o365DistributionGroup -identity $groupSMTPAddress -RecipientTypeDetails $functionMailSecurity -errorAction STOP
-
-                    out-logfile -string "Original DL configuration found and recorded by filter security."
-                }
-                elseif ($groupTypeOverride -eq "Distribution")
-                {
-                    out-logfile -string "Using Exchange Online to capture distribution group with filter distribution."
-
-                    $functionDLConfiguration=get-o365DistributionGroup -identity $groupSMTPAddress -RecipientTypeDetails $functionMailDistribution
-
-                    out-logfile -string "Original DL configuration found and recorded by filter distribution."
-                }
-                
-            }
-            catch 
-            {
-                Out-LogFile -string $_ -isError:$TRUE
-            }
-        }
-        else
-        {
-            out-logfile -string "Group is unified use unified group commands."
-
-            try
-            {
-                $functionDLConfiguration = get-o365UnifiedGroup -identity $groupSMTPAddress -includeAllProperties -errorAction STOP
-            }
-            catch
-            {
-                out-logfile -string $_ -isError:$TRUE
-            }
+        catch {
+            out-logfile -string "Unable to obtain the Office 365 Distribution Group."
+            out-logfile -string $_ -isError:$true
         }
         
         
