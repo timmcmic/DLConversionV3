5238fb3dccc1fd6dcc56dfd503ea2ec0fee2fb99
Timothy J. McMichael <timmcmic@microsoft.com>
2025-08-04 12:06:04 -0400

    UPdating

--- start-parameterValidationGraph.ps1
+++ start-parameterValidationGraph.ps1
@@ -32,9 +32,56 @@
 
         out-logfile -string "Validate that only a single graph credetial type is in use."
 
-        if (($msGraphClientSecret -ne "") -and ($msGraphCertificateThumbprint -ne "")
+        if (($msGraphClientSecret -ne "") -and ($msGraphCertificateThumbprint -ne ""))
         {
-            out-logfile -string "A client secret and certificate thumbprint are specified - utilize only one method for graph app authentication."
+            out-logfile -string "A client secret and certificate thumbprint are specified - utilize only one method for graph app authentication." -isError:$TRUE
+        }
+        else 
+        {
+            out-logfile -string "Proceed with authentication method verification."
+        }
+
+        if ($msGraphClientSecret -ne "")
+        {
+            out-logfile -string "Client secret authentication specified."
+
+            if (($msGraphTenantID -eq "") -and ($msGraphApplicationID -eq ""))
+            {
+                out-logfile -string "When specifying a graph client secret tenant ID and application ID are required." -isError:$TRUE
+            }
+            elseif(($msGraphTenantID -ne "") -and ($msGraphApplicationID -eq ""))
+            {
+                out-logfile -string "When specifying a graph client secret and tenant ID an application ID is required." -isError:$TRUE
+            }
+            elseif(($msGraphTenantID -eq "") -and ($msGraphApplicationID -ne ""))
+            {
+                out-logfile -string "When specifying a graph client secret and application ID the tenant ID is required." -isError:$TRUE
+            }
+            else 
+            {
+                out-logfile -string "All components for graph client secret authentication are present."
+            }
+        }
+        elseif ($msGraphCertificateThumbprint -ne "")
+        {
+            out-logfile -string "Certificate authentication specified."
+
+            if (($msGraphTenantID -eq "") -and ($msGraphApplicationID -eq ""))
+            {
+                out-logfile -string "When specifying a graph certificate thumbprint tenant ID and application ID are required." -isError:$TRUE
+            }
+            elseif(($msGraphTenantID -ne "") -and ($msGraphApplicationID -eq ""))
+            {
+                out-logfile -string "When specifying a graph certificate thumbprint and tenant ID an application ID is required." -isError:$TRUE
+            }
+            elseif(($msGraphTenantID -eq "") -and ($msGraphApplicationID -ne ""))
+            {
+                out-logfile -string "When specifying a graph certificate thumbprint and application ID the tenant ID is required." -isError:$TRUE
+            }
+            else 
+            {
+                out-logfile -string "All components for graph certificate authentication are present."
+            }
         }
 
         Out-LogFile -string "********************************************************************************"
