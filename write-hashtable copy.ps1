fc6a7c79c381d75996a21e4ac1059839e097f46e
Timothy J. McMichael <timmcmic@microsoft.com>
2025-08-04 21:46:48 -0400

    Updating

---
+++ write-hashtable copy.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+
+This function adds the routing contact to the on premises group to maintain mail flow.
+
+.DESCRIPTION
+
+This function adds the routing contact to the on premises group to maintain mail flow.
+
+
+#>
+Function add-RoutingContactToGroup
+{
+    [cmdletbinding()]
+
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        $routingContact,
+        [Parameter(Mandatory = $true)]
+        $originalDLConfiguration
+    )
+
+    Out-LogFile -string "********************************************************************************"
+    Out-LogFile -string "Start add-RoutingContactToGroup"
+    Out-LogFile -string "********************************************************************************"
+
+
+    foreach ($key in $hashtable.GetEnumerator())
+    {
+        out-logfile -string ("Key: "+$key.name+" is "+$key.Value.Description+" with value "+$key.Value.Value)
+    }      
+
+    out-LogFile -string "********************************************************************************"
+    Out-LogFile -string "End add-RoutingContactToGroup"
+    Out-LogFile -string "********************************************************************************"}
