a08084d2e1a8b80cb5fc3a46edb49f0bb9a5ebbc
Timothy J. McMichael <timmcmic@microsoft.com>
2025-08-12 20:33:44 -0400

    Updating

--- validate-ADRecycleBinEnabled.ps1
+++ validate-ADRecycleBinEnabled.ps1
@@ -11,9 +11,6 @@
     Function validate-ADRecycleBinEnabled
     {
         $functionTest = $null
-        $functionGood = "Good"
-        $functionBad = "Bad"
-        $functionResult = ""
 
         Out-LogFile -string "********************************************************************************"
         Out-LogFile -string "START validate-ADRecycleBinDisabled"
@@ -28,29 +25,10 @@
             out-logfile -string "Unable to obtain the ad recycle bin info."
             out-logfile -string $_ -isError:$TRUE
         }
-    
-
-        if (($functionTest.enabledScopes.count -gt 0) -or ($funcionTest.enabledScopes -ne $null))
-        {
-            out-logfile -string "Enabled scopes are present - this signifies recycle bin is enabled."
-
-            foreach ($scope in $functionTest.enabledScopes)
-            {
-                out-logfile -string $scope
-            }
-
-            $functionResult = $functionGood
-        }
-        else 
-        {
-            out-logfile -string "Recycle bin is not enabled - not good for this scenario."
-
-            $functionResult = $functionBad
-        }
 
         Out-LogFile -string "********************************************************************************"
         Out-LogFile -string "END validate-ADRecycleBinDisabled"
         Out-LogFile -string "********************************************************************************"
 
-        return $functionResult
+        return $functionTest
     }
